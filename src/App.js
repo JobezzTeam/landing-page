56f04a35e32a674277a346dfd023714d427a89e8
iChrOniiK <theo.lebarq@epitech.eu>
2020-01-28 17:52:47 +0100

    [ADD] - Setup Project, and create files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Presentation } from '../pages/index'
+
+function App() {
+  return (
+    <div className="App">
+      <Presentation/>
+    </div>
+  );
+}
+
+export default App;
